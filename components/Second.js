67c9e1c66df56bde1d9e8e9193423455b5cc8ac6
Harshdeep <harshdeepraghuwanshi91@gmail.com>
2022-03-12 18:28:02 +0530

    implemented second page

--- components/Second.js
+++ components/Second.js
@@ -11,12 +11,56 @@ const Second = () => {
                       </div>
               </div>
               {/* second section */}
-              <div>
+              <div className={styles.group8}>
+                  <div className={styles.group2}>
+                      <div className={styles.benefit2}>Benefit</div>
+                      <div className={styles.coffeePoti}></div>
+                      <div className={styles.speakingab}>
+                      Speaking about comparison of the matter of the prominent landmarks and competitive development and manufacturing, the matters of peculiar interest and growth opportunities of it are quite high. 
+                      </div>
+                  </div>
+                  <div className={styles.group3}>
+                      <div className={styles.benefit3}>Benefit</div>
+                      <div className={styles.coffeeBean}></div>
+                      <div className={styles.thoughtheo}>
+                      Speaking about comparison of the matter of the prominent landmarks and competitive development and manufacturing, the matters of peculiar interest and growth opportunities of it are quite high.
+                      </div>
+                  </div>
+                  <div className={styles.group4}>
+                      <div className={styles.benefit4}>Benefit</div>
+                      <div className={styles.coffeeToGo}></div>
+                      <div className={styles.throughout}>
+                      Speaking about comparison of the matter of the prominent landmarks and competitive development and manufacturing, the matters of peculiar interest and growth opportunities of it are quite high.
+                      </div>
+                  </div>
+                  <div className={styles.group5}>
+                      <div className={styles.benefit5}>Benefit</div>
+                      <div className={styles.coffeeMake}></div>
+                      <div className={styles.tobequitef}>
+                      Speaking about comparison of the matter of the prominent landmarks and competitive development and manufacturing, the matters of peculiar interest and growth opportunities of it are quite high.
+                      </div>
+                  </div>
+                  <div className={styles.group6}>
+                      <div className={styles.benefit6}>Benefit</div>
+                      <div className={styles.cafeicon}></div>
+                      <div className={styles.tobequitef6}>
+                      Speaking about comparison of the matter of the prominent landmarks and competitive development and manufacturing, the matters of peculiar interest and growth opportunities of it are quite high.
+                      </div>
+                  </div>
+                  <div className={styles.group7}>
+                      <div className={styles.benefit7}>Benefit</div>
+                      <div className={styles.hotCoffeei}></div>
+                      <div className={styles.tobequitef}>
+                      Speaking about comparison of the matter of the prominent landmarks and competitive development and manufacturing, the matters of peculiar interest and growth opportunities of it are quite high.
+                      </div>
+                  </div>
+                  
                   
               </div>
               {/* Third section */}
               <div>
-
+                  <div className={styles.expandArro}></div>
+                  <div className={styles.roastingOn}>Roasting Only The Finest Beans Since 1973</div>
               </div>
           </div>
     </div>
