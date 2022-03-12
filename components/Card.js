74834eb132726c6c6b4aad53178323853b5b255b
Harshdeep <harshdeepraghuwanshi91@gmail.com>
2022-03-12 20:17:09 +0530

    implemented a lot of things

---
+++ components/Card.js
@@ -0,0 +1,16 @@
+
+const card = (styles) => {
+  return (
+    <div className={styles.group}>
+            <div className={styles.rectangle}>
+              
+              <div className={styles.avatar}></div>
+              <div className={styles.name}>Jhon Cena</div>
+              <div className={styles.desc}>Let it not be said that the results of the arguments and claims definitely illustrates the utter importance of The Environment of Handy Keynote </div>
+              
+            </div>  
+        </div>
+  )
+}
+
+export default card
--- components/Fourth.js
+++ components/Fourth.js
@@ -1,7 +1,37 @@
 import styles from '../styles/Fourth.module.css'
+import Card from './Card'
 const Fourth = () => {
+
+  const cards = () => {
+    const array = [1, 2, 3, 4, 5];
+    return array.map(number =>
+       <Card key={number}
+        group={styles.group}
+        rectangle={styles.rectangle}
+        avatar={styles.avatar}
+        name={styles.name}
+        desc={styles.desc} />
+    )
+  }
+  const Abouts = () => {
+    const array = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+    return array.map(() =><h1>About Us</h1>
+    )
+  }
   return (
-    <div>Fourth</div>
+    <div>
+      <div className={styles.whatOurCus}>
+      What Our Customers Says,
+      </div>
+      <div className={styles.group22}>
+        {cards()}
+      </div>
+      <div className={styles.rectangleC}>
+        <div className={styles.about}>
+          {Abouts()}
+          </div>
+      </div>
+    </div>
   )
 }
 
