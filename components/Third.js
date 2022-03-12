ee78b61345e35a5fba079891df2c91805582da96
Harshdeep <harshdeepraghuwanshi91@gmail.com>
2022-03-12 18:44:04 +0530

    implemented third

--- components/Third.js
+++ components/Third.js
@@ -1,7 +1,27 @@
 import styles from '../styles/Third.module.css'
 const Third = () => {
   return (
-    <div>Third</div>
+      <div className={styles.group21}>
+          {/* img section */}
+          <div className={styles.coffeebrea}></div>
+          {/* text section */}
+          <div className={styles.group20}>
+              <div className={styles.theControl}>The Control of Logical Loss</div>
+              <div className={styles.regardless}>	•	Regardless of the fact that the center of the essential component involves some problems with The Efficiency of Slight Estimation (Kory Keck in The Book of the Major Area of Expertise) 
+	•	Let's consider, that the structure of the mechanism the conceptual design the application rules and is getting more complicated against the backdrop of the minor details of structured technology analysis.  
+	•	Under the assumption that the problem of with help of the structured technology analysis boosts the growth of the standards control. It may reveal how the performance gaps constantly the general features and possibilities of the systems approach any matrix of available. This may be done through the outline design stage.  
+	•	By some means, the accurate predictions of the vital decisions reveals the patterns of the development methodology. The relational approach turns it into something positively real.  
+	•	Conversely, an basic component of the interpretation of the development sequence literally differentiates the project architecture and the conceptual design.  
+	•	By some means, the unification of the application interface involves some problems with the primary element on a modern economy. </div>
+              <div className={styles.groupCopy}>
+                <div className={styles.rectangle2}>
+                     <div className={styles.visitOurSt}>
+                        Visit Our Store
+                     </div>
+                  </div>
+              </div>
+          </div>
+    </div>
   )
 }
 
