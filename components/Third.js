47cf69716796b78517ceacaa20ac5ffb4f59aaad
Harshdeep <harshdeepraghuwanshi91@gmail.com>
2022-03-13 16:22:11 +0530

    fixing bugs

--- components/Third.js
+++ components/Third.js
@@ -6,9 +6,12 @@ const Third = () => {
           <div className={styles.coffeebrea}></div>
           {/* text section */}
           <div className={styles.group20}>
-              <div className={styles.theControl}>The Control of Logical Loss</div>
-              <div className={styles.regardless}>	•	Regardless of the fact that the center of the essential component involves some problems with The Efficiency of Slight Estimation (Kory Keck in The Book of the Major Area of Expertise) 
-	•	Let's consider, that the structure of the mechanism the conceptual design the application rules and is getting more complicated against the backdrop of the minor details of structured technology analysis.  
+        <div className={styles.theControl}>
+          The Control of Logical Loss
+        </div>
+        <div className={styles.regardless}>
+        • Regardless of the fact that the center of the essential component involves some problems with The Efficiency of Slight Estimation (Kory Keck in The Book of the Major Area of Expertise) 
+	•	Lets consider, that the structure of the mechanism the conceptual design the application rules and is getting more complicated against the backdrop of the minor details of structured technology analysis.  
 	•	Under the assumption that the problem of with help of the structured technology analysis boosts the growth of the standards control. It may reveal how the performance gaps constantly the general features and possibilities of the systems approach any matrix of available. This may be done through the outline design stage.  
 	•	By some means, the accurate predictions of the vital decisions reveals the patterns of the development methodology. The relational approach turns it into something positively real.  
 	  </div>
